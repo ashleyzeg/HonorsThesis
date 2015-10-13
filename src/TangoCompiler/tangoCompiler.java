f85f86e16e0f68fedcc8435fc7c4ef4d14a0d573
ashleyzeg <ashleyzeg@gmail.com>
2015-10-13 18:46:05 -0400

    added main program and scanner to version control

---
+++ src/TangoCompiler/tangoCompiler.java
@@ -0,0 +1,30 @@
+package TangoCompiler;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Created by azegiest on 10/13/15.
+ */
+public class tangoCompiler {
+    public static void main(String [] args) throws IOException {
+
+        System.out.println("*** Welcome to the Tango Compiler!!! ***");
+        System.out.println("Enter the file you would like to compile and run:");
+
+        Scanner in = new Scanner(System.in);
+        File inFile = new File("");
+        File completePath = new File(inFile.getAbsolutePath() + "/src/TangoCompiler", in.nextLine());
+
+        if (completePath.exists() && !completePath.isDirectory()) {
+            inFile = completePath;
+        } else {
+            System.out.println("Invalid file name, or file does not exist!");
+            System.exit(0);
+        }
+
+        tangoScanner ts = new tangoScanner(inFile);
+
+    }
+}
---
+++ src/TangoCompiler/tangoScanner.java
@@ -0,0 +1,14 @@
+package TangoCompiler;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * Created by azegiest on 10/13/15.
+ */
+public class tangoScanner {
+    public tangoScanner(File input) throws FileNotFoundException {
+        System.out.println("*** Welcome to the Tango Scanner ***");
+
+    }
+}
